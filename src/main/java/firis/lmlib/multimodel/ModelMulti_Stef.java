4301bc2fe6b85b1464517b791fb86122c1418f0b
firis <firis.games.lapismoonlight@gmail.com>
2020-07-21 23:16:35 +0900

    Steve/Stefモデル調整
    ・お座りモーション位置調整
    ・ソースコード整理

--- src/main/java/firis/lmlib/multimodel/ModelMulti_Stef.java
+++ src/main/java/firis/lmlib/multimodel/ModelMulti_Stef.java
@@ -3,6 +3,12 @@ package firis.lmlib.multimodel;
 import firis.lmmm.api.caps.IModelCaps;
 import firis.lmmm.api.renderer.ModelRenderer;
 
+/**
+ * Stefマルチモデル
+ * 
+ * @author firis-games
+ *
+ */
 public class ModelMulti_Stef extends ModelMulti_Steve {
 
 	public ModelRenderer bust;
@@ -15,8 +21,7 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		super(psize);
 	}
 
-	public ModelMulti_Stef(float psize, float pyoffset, int pTextureWidth,
-			int pTextureHeight) {
+	public ModelMulti_Stef(float psize, float pyoffset, int pTextureWidth, int pTextureHeight) {
 		super(psize, pyoffset, 64, 32);
 	}
 
@@ -26,28 +31,28 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		bipedCloak.addBox(-5.0F, 0.0F, -1.0F, 10, 16, 1, size);
 		bipedEars = new ModelRenderer(this, 24, 0);
 		bipedEars.addBox(-3.0F, -6.0F, -1.0F, 6, 6, 1, size);
-		
+
 		bipedHead = new ModelRenderer(this, 0, 0);
 		bipedHead.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, size - 0.2F);
 		bipedHead.setRotationPoint(0.0F, 1.35F, 0.0F);
 		bipedHeadwear = new ModelRenderer(this, 32, 0);
 		bipedHeadwear.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, size + 0.3F);
 		bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
-//		eyeL = new ModelRenderer(this, 0, 0);
-//		eyeL.addBox(0.0F, -5F, -4.001F, 4, 4, 0, size - 0.2F);
-//		eyeL.setRotationPoint(0.0F, 0.0F, 0.0F);
-//		eyeR = new ModelRenderer(this, 0, 4);
-//		eyeR.addBox(-4F, -5F, -4.001F, 4, 4, 0, size - 0.2F);
-//		eyeR.setRotationPoint(0.0F, 0.0F, 0.0F);
-		
+		// eyeL = new ModelRenderer(this, 0, 0);
+		// eyeL.addBox(0.0F, -5F, -4.001F, 4, 4, 0, size - 0.2F);
+		// eyeL.setRotationPoint(0.0F, 0.0F, 0.0F);
+		// eyeR = new ModelRenderer(this, 0, 4);
+		// eyeR.addBox(-4F, -5F, -4.001F, 4, 4, 0, size - 0.2F);
+		// eyeR.setRotationPoint(0.0F, 0.0F, 0.0F);
+
 		bipedBody = new ModelRenderer(this, 16, 16);
 		bipedBody.addBox(-4.0F, 0.2F, -2.0F, 8, 12, 4, size - 0.3F);
 		bipedBody.setRotationPoint(0.0F, 0.7F, 0.0F);
 		bust = new ModelRenderer(this, 16, 21);
-		bust.addBox(-4F, -2F, -2F, 8, 4, 4, size/* - 0.3001F*/);
+		bust.addBox(-4F, -2F, -2F, 8, 4, 4, size/* - 0.3001F */);
 		bust.setScale(0.9f, 0.8f, 0.8f);
 		bust.setRotationPoint(0.0F, 4.25F, -1.4F).setRotateAngleDeg(50F, 0F, 0F);
-		
+
 		bipedRightArm = new ModelRenderer(this, 40, 16);
 		bipedRightArm.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, size - 0.4F);
 		bipedRightArm.setRotationPoint(-4.4F, 2.85F, 0.0F);
@@ -55,7 +60,7 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		bipedLeftArm.mirror = true;
 		bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, size - 0.4F);
 		bipedLeftArm.setRotationPoint(4.4F, 2.85F, 0.0F);
-		
+
 		bipedRightLeg = new ModelRenderer(this, 0, 16);
 		bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, size - 0.3F);
 		bipedRightLeg.setRotationPoint(-1.7F, 12.3F, 0.0F);
@@ -63,19 +68,18 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		bipedLeftLeg.mirror = true;
 		bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, size - 0.3F);
 		bipedLeftLeg.setRotationPoint(1.7F, 12.3F, 0.0F);
-		
+
 		HeadMount.setRotationPoint(0.0F, -4.0F, 0.0F);
 		HeadTop.setRotationPoint(0.0F, -12.0F, 0.0F);
 		Arms[0] = new ModelRenderer(this);
 		Arms[0].setRotationPoint(-1.5F, 7.2F, -1F);
 		Arms[1] = new ModelRenderer(this);
 		Arms[1].setRotationPoint(1.5F, 7.2F, -1F);
-		
+
 		bipedTorso = new ModelRenderer(this);
 		bipedNeck = new ModelRenderer(this);
 		bipedPelvic = new ModelRenderer(this);
 
-		
 		mainFrame = new ModelRenderer(this);
 		mainFrame.setRotationPoint(0F, yOffset, 0F);
 		mainFrame.addChild(bipedTorso);
@@ -87,8 +91,8 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		bipedHead.addChild(bipedEars);
 		bipedHead.addChild(HeadMount);
 		bipedHead.addChild(HeadTop);
-//		bipedHead.addChild(eyeL);
-//		bipedHead.addChild(eyeR);
+		// bipedHead.addChild(eyeL);
+		// bipedHead.addChild(eyeR);
 		bipedNeck.addChild(bipedRightArm);
 		bipedNeck.addChild(bipedLeftArm);
 		bipedPelvic.addChild(bipedRightLeg);
@@ -97,22 +101,25 @@ public class ModelMulti_Stef extends ModelMulti_Steve {
 		bipedLeftArm.addChild(Arms[1]);
 		bipedBody.addChild(bust);
 		bipedBody.addChild(bipedCloak);
-		
+
 		bipedEars.showModel = false;
 		bipedCloak.showModel = false;
-		
+
 	}
 
-	@Override
-	public String getUsingTexture() {
-		return "Crafter";
-	}
+	// @Override
+	// public String getUsingTexture() {
+	// return "Crafter";
+	// }
 
-	public void setDefaultPause(float par1, float par2, float pTicksExisted,
-			float pHeadYaw, float pHeadPitch, float par6, IModelCaps pEntityCaps) {
+	public void setDefaultPause(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch, float scaleFactor, IModelCaps entityCaps) {
+
+		mainFrame.setRotationPoint(0F, 0F, 0F);
+
 		// 初期姿勢
 		bipedBody.setRotationPoint(0.0F, 0.7F, 0.0F).setRotateAngle(0.0F, 0.0F, 0.0F);
-		bipedHead.setRotationPoint(0.0F, 1.35F, 0.0F).setRotateAngleDeg(pHeadPitch, pHeadYaw, 0.0F);
+		bipedHead.setRotationPoint(0.0F, 1.35F, 0.0F).setRotateAngleDeg(headPitch, netHeadYaw, 0.0F);
 		bipedRightArm.setRotationPoint(-4.4F, 2.85F, 0.0F).setRotateAngleDeg(0.0F, 0.0F, 10.0F);
 		bipedLeftArm.setRotationPoint(4.4F, 2.85F, 0.0F).setRotateAngleDeg(0.0F, 0.0F, -10.0F);
 		bipedRightLeg.setRotationPoint(-2.3F, 11.5F, 0.0F).setRotateAngleDeg(0.0F, 0.0F, -2.0F);
--- src/main/java/firis/lmlib/multimodel/ModelMulti_Steve.java
+++ src/main/java/firis/lmlib/multimodel/ModelMulti_Steve.java
@@ -5,8 +5,15 @@ import org.lwjgl.opengl.GL11;
 import firis.lmmm.api.caps.IModelCaps;
 import firis.lmmm.api.caps.ModelCapsHelper;
 import firis.lmmm.api.model.ModelMultiBase;
+import firis.lmmm.api.model.motion.LMMotionSitdown;
 import firis.lmmm.api.renderer.ModelRenderer;
 
+/**
+ * Steveマルチモデル
+ * 
+ * @author firis-games
+ *
+ */
 public class ModelMulti_Steve extends ModelMultiBase {
 
 	public ModelRenderer bipedHead;
@@ -21,17 +28,18 @@ public class ModelMulti_Steve extends ModelMultiBase {
 	public ModelRenderer bipedTorso;
 	public ModelRenderer bipedNeck;
 	public ModelRenderer bipedPelvic;
-	
-//	public ModelRenderer eyeR;
-//	public ModelRenderer eyeL;
 
+	// public ModelRenderer eyeR;
+	// public ModelRenderer eyeL;
 
 	public ModelMulti_Steve() {
 		super();
 	}
+
 	public ModelMulti_Steve(float psize) {
 		super(psize);
 	}
+
 	public ModelMulti_Steve(float psize, float pyoffset, int pTextureWidth, int pTextureHeight) {
 		super(psize, pyoffset, pTextureWidth, pTextureHeight);
 	}
@@ -42,20 +50,20 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		bipedCloak.addBox(-5.0F, 0.0F, -1.0F, 10, 16, 1, size);
 		bipedEars = new ModelRenderer(this, 24, 0);
 		bipedEars.addBox(-3.0F, -6.0F, -1.0F, 6, 6, 1, size);
-		
+
 		bipedHead = new ModelRenderer(this, 0, 0);
 		bipedHead.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, size);
 		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
 		bipedHeadwear = new ModelRenderer(this, 32, 0);
 		bipedHeadwear.addBox(-4.0F, -8.0F, -4.0F, 8, 8, 8, size + 0.5F);
 		bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
-//		eyeL = new ModelRenderer(this, 0, 0);
-//		eyeL.addBox(0.0F, -5F, -4.001F, 4, 4, 0, size);
-//		eyeL.setRotationPoint(0.0F, 0.0F, 0.0F);
-//		eyeR = new ModelRenderer(this, 0, 4);
-//		eyeR.addBox(-4F, -5F, -4.001F, 4, 4, 0, size);
-//		eyeR.setRotationPoint(0.0F, 0.0F, 0.0F);
-		
+		// eyeL = new ModelRenderer(this, 0, 0);
+		// eyeL.addBox(0.0F, -5F, -4.001F, 4, 4, 0, size);
+		// eyeL.setRotationPoint(0.0F, 0.0F, 0.0F);
+		// eyeR = new ModelRenderer(this, 0, 4);
+		// eyeR.addBox(-4F, -5F, -4.001F, 4, 4, 0, size);
+		// eyeR.setRotationPoint(0.0F, 0.0F, 0.0F);
+
 		bipedBody = new ModelRenderer(this, 16, 16);
 		bipedBody.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, size);
 		bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
@@ -67,7 +75,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		bipedLeftArm.mirror = true;
 		bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, size);
 		bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
-		
+
 		bipedRightLeg = new ModelRenderer(this, 0, 16);
 		bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, size);
 		bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
@@ -75,18 +83,18 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		bipedLeftLeg.mirror = true;
 		bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, size);
 		bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
-		
+
 		HeadMount.setRotationPoint(0.0F, -4.0F, 0.0F);
 		HeadTop.setRotationPoint(0.0F, -12.0F, 0.0F);
 		Arms[0] = new ModelRenderer(this);
 		Arms[0].setRotationPoint(-1.5F, 7.2F, -1F);
 		Arms[1] = new ModelRenderer(this);
 		Arms[1].setRotationPoint(1.5F, 7.2F, -1F);
-		
+
 		bipedTorso = new ModelRenderer(this);
 		bipedNeck = new ModelRenderer(this);
 		bipedPelvic = new ModelRenderer(this);
-		
+
 		mainFrame = new ModelRenderer(this);
 		mainFrame.setRotationPoint(0F, yOffset, 0F);
 		mainFrame.addChild(bipedTorso);
@@ -98,8 +106,8 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		bipedHead.addChild(bipedEars);
 		bipedHead.addChild(HeadMount);
 		bipedHead.addChild(HeadTop);
-//		bipedHead.addChild(eyeL);
-//		bipedHead.addChild(eyeR);
+		// bipedHead.addChild(eyeL);
+		// bipedHead.addChild(eyeR);
 		bipedNeck.addChild(bipedRightArm);
 		bipedNeck.addChild(bipedLeftArm);
 		bipedPelvic.addChild(bipedRightLeg);
@@ -107,24 +115,28 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		bipedRightArm.addChild(Arms[0]);
 		bipedLeftArm.addChild(Arms[1]);
 		bipedBody.addChild(bipedCloak);
-		
+
 		bipedEars.showModel = false;
 		bipedCloak.showModel = false;
-		
+
 	}
 
 	@Override
 	public void render(IModelCaps entityCaps, float limbSwing, float limbSwingAmount, float ageInTicks,
 			float netHeadYaw, float headPitch, float scale, boolean isRender) {
-		setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityCaps);
+		// setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw,
+		// headPitch, scale, entityCaps);
 		mainFrame.render(scale, isRender);
 	}
 
-	public void setDefaultPause(float par1, float par2, float pTicksExisted,
-			float pHeadYaw, float pHeadPitch, float par6, IModelCaps pEntityCaps) {
+	public void setDefaultPause(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch, float scaleFactor, IModelCaps entityCaps) {
+
+		mainFrame.setRotationPoint(0F, 0F, 0F);
+
 		// 初期姿勢
 		bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F).setRotateAngle(0.0F, 0.0F, 0.0F);
-		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F).setRotateAngleDeg(pHeadPitch, pHeadYaw, 0.0F);
+		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F).setRotateAngleDeg(headPitch, netHeadYaw, 0.0F);
 		bipedRightArm.setRotationPoint(-5.0F, 2.0F, 0.0F).setRotateAngle(0.0F, 0.0F, 0.0F);
 		bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F).setRotateAngle(0.0F, 0.0F, 0.0F);
 		bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F).setRotateAngle(0.0F, 0.0F, 0.0F);
@@ -135,10 +147,10 @@ public class ModelMulti_Steve extends ModelMultiBase {
 	}
 
 	@Override
-	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks,
-			float netHeadYaw, float headPitch, float scaleFactor, IModelCaps entityCaps) {
+	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch, float scaleFactor, IModelCaps entityCaps) {
 		setDefaultPause(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityCaps);
-		
+
 		// 腕ふり、腿上げ
 		float lf1 = mh_cos(limbSwing * 0.6662F);
 		float lf2 = mh_cos(limbSwing * 0.6662F + PI);
@@ -146,8 +158,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		this.bipedLeftArm.rotateAngleX = lf1 * 2.0F * limbSwingAmount * 0.5F;
 		this.bipedRightLeg.rotateAngleX = lf1 * 1.4F * limbSwingAmount;
 		this.bipedLeftLeg.rotateAngleX = lf2 * 1.4F * limbSwingAmount;
-		
-		
+
 		if (isRiding) {
 			bipedRightArm.addRotateAngleDegX(-36.0F);
 			bipedLeftArm.addRotateAngleDegX(-36.0F);
@@ -156,7 +167,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 			bipedRightLeg.addRotateAngleDegY(18.0F);
 			bipedLeftLeg.addRotateAngleDegY(-18.0F);
 		}
-		
+
 		if (heldItem[0] > 0) {
 			bipedRightArm.rotateAngleX = bipedRightArm.rotateAngleX * 0.5F;
 			bipedRightArm.addRotateAngleDegX(-18.0F * heldItem[0]);
@@ -165,23 +176,23 @@ public class ModelMulti_Steve extends ModelMultiBase {
 			bipedLeftArm.rotateAngleX = bipedLeftArm.rotateAngleX * 0.5F;
 			bipedLeftArm.addRotateAngleDegX(-18.0F * heldItem[1]);
 		}
-		
+
 		float lf;
 		if ((onGrounds[0] > -9990F || onGrounds[1] > -9990F) && !aimedBow) {
 			// 腕振り
-			lf = (float)Math.PI * 2.0F;
+			lf = (float) Math.PI * 2.0F;
 			lf1 = mh_sin(mh_sqrt(onGrounds[0]) * lf);
 			lf2 = mh_sin(mh_sqrt(onGrounds[1]) * lf);
 			bipedTorso.rotateAngleY = (lf1 - lf2) * 0.2F;
-			
+
 			// R
 			if (onGrounds[0] > 0F) {
 				lf = 1.0F - onGrounds[0];
 				lf *= lf;
 				lf *= lf;
 				lf = 1.0F - lf;
-				lf1 = mh_sin(lf * (float)Math.PI);
-				lf2 = mh_sin(onGrounds[0] * (float)Math.PI) * -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
+				lf1 = mh_sin(lf * (float) Math.PI);
+				lf2 = mh_sin(onGrounds[0] * (float) Math.PI) * -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
 				bipedRightArm.addRotateAngleX(-lf1 * 1.2F - lf2);
 				bipedRightArm.addRotateAngleY(bipedTorso.rotateAngleY * 2.0F);
 				bipedRightArm.setRotateAngleZ(mh_sin(onGrounds[0] * 3.141593F) * -0.4F);
@@ -194,8 +205,8 @@ public class ModelMulti_Steve extends ModelMultiBase {
 				lf *= lf;
 				lf *= lf;
 				lf = 1.0F - lf;
-				lf1 = mh_sin(lf * (float)Math.PI);
-				lf2 = mh_sin(onGrounds[1] * (float)Math.PI) * -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
+				lf1 = mh_sin(lf * (float) Math.PI);
+				lf2 = mh_sin(onGrounds[1] * (float) Math.PI) * -(bipedHead.rotateAngleX - 0.7F) * 0.75F;
 				bipedLeftArm.addRotateAngleX(-lf1 * 1.2F - lf2);
 				bipedLeftArm.addRotateAngleY(bipedTorso.rotateAngleY * 2.0F);
 				bipedLeftArm.setRotateAngleZ(mh_sin(onGrounds[1] * 3.141593F) * 0.4F);
@@ -203,7 +214,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 				bipedLeftArm.rotateAngleX += bipedTorso.rotateAngleY;
 			}
 		}
-		
+
 		if (isSneak) {
 			// しゃがみ
 			bipedBody.rotationPointY = 2.0F;
@@ -219,7 +230,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 			// 高さ調整
 			bipedTorso.rotationPointY += 1.2F;
 		}
-		
+
 		if (aimedBow) {
 			lf1 = 0.0F;
 			lf2 = 0.0F;
@@ -228,7 +239,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 			lf = 0.1F - lf1 * 0.6F;
 			bipedRightArm.rotateAngleY = -lf + bipedHead.rotateAngleY;
 			bipedLeftArm.rotateAngleY = lf + bipedHead.rotateAngleY;
-			lf = (float)Math.PI * 0.5F;
+			lf = (float) Math.PI * 0.5F;
 			bipedRightArm.rotateAngleX = -lf + bipedHead.rotateAngleX;
 			bipedLeftArm.rotateAngleX = -lf + bipedHead.rotateAngleX;
 			bipedRightArm.rotateAngleX -= lf1 * 1.2F - lf2 * 0.4F;
@@ -239,7 +250,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 				bipedRightArm.rotateAngleY += 0.4F;
 			}
 		}
-		
+
 		// 腕の揺らぎ
 		lf = mh_cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
 		this.bipedRightArm.rotateAngleZ += lf;
@@ -247,33 +258,41 @@ public class ModelMulti_Steve extends ModelMultiBase {
 		lf = mh_sin(ageInTicks * 0.067F) * 0.05F;
 		this.bipedRightArm.rotateAngleX += lf;
 		this.bipedLeftArm.rotateAngleX -= lf;
-		
+
+		// 追加モーション（お座り）
+		if (this.isRiding && LMMotionSitdown.SITDOWN
+				.equals((String) entityCaps.getCapsValue(IModelCaps.caps_multimodel_motion))) {
+			this.mainFrame.rotationPointY += 7.00F;
+		}
+
 	}
 
-//	@Override
-//	public void renderItems(IModelCaps pEntityCaps) {
-//		// 手持ちの表示
-//		GL11.glPushMatrix();
-//		
-//		// R
-//		Arms[0].loadMatrix();
-////		GL11.glTranslatef(0F, 0.05F, -0.05F);
-//		Arms[0].renderItems(this, pEntityCaps, false, 0);
-//		// L
-//		Arms[1].loadMatrix();
-////		GL11.glTranslatef(0F, 0.05F, -0.05F);
-//		Arms[1].renderItems(this, pEntityCaps, false, 1);
-//		// 頭部装飾品
-//		boolean lplanter = ModelCapsHelper.getCapsValueBoolean(pEntityCaps, caps_isPlanter);
-//		if (ModelCapsHelper.getCapsValueBoolean(pEntityCaps, caps_isCamouflage) || lplanter) {
-//			if (lplanter) {
-//				HeadTop.loadMatrix().renderItemsHead(this, pEntityCaps);
-//			} else {
-//				HeadMount.loadMatrix().renderItemsHead(this, pEntityCaps);
-//			}
-//		}
-//		GL11.glPopMatrix();
-//	}
+	// @Override
+	// public void renderItems(IModelCaps pEntityCaps) {
+	// // 手持ちの表示
+	// GL11.glPushMatrix();
+	//
+	// // R
+	// Arms[0].loadMatrix();
+	//// GL11.glTranslatef(0F, 0.05F, -0.05F);
+	// Arms[0].renderItems(this, pEntityCaps, false, 0);
+	// // L
+	// Arms[1].loadMatrix();
+	//// GL11.glTranslatef(0F, 0.05F, -0.05F);
+	// Arms[1].renderItems(this, pEntityCaps, false, 1);
+	// // 頭部装飾品
+	// boolean lplanter = ModelCapsHelper.getCapsValueBoolean(pEntityCaps,
+	// caps_isPlanter);
+	// if (ModelCapsHelper.getCapsValueBoolean(pEntityCaps, caps_isCamouflage) ||
+	// lplanter) {
+	// if (lplanter) {
+	// HeadTop.loadMatrix().renderItemsHead(this, pEntityCaps);
+	// } else {
+	// HeadMount.loadMatrix().renderItemsHead(this, pEntityCaps);
+	// }
+	// }
+	// GL11.glPopMatrix();
+	// }
 
 	@Override
 	public void renderFirstPersonHand(IModelCaps pEntityCaps) {
@@ -287,7 +306,7 @@ public class ModelMulti_Steve extends ModelMultiBase {
 
 	@Override
 	public float[] getArmorModelsSize() {
-		return new float[] {0.5F, 1.0F};
+		return new float[] { 0.5F, 1.0F };
 	}
 
 	@Override
@@ -317,7 +336,6 @@ public class ModelMulti_Steve extends ModelMultiBase {
 
 	@Override
 	public float getLeashOffset(IModelCaps pEntityCaps) {
-		// TODO Auto-generated method stub
 		return 0.2F;
 	}
 
