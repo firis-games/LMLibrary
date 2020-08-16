d11cfe36293a783e215a11f338649b75ec9fbe15
firis <firis.games.lapismoonlight@gmail.com>
2020-08-16 23:26:07 +0900

    ver1.0.8

--- src/main/java/firis/lmlib/LMLibrary.java
+++ src/main/java/firis/lmlib/LMLibrary.java
@@ -52,7 +52,7 @@ public class LMLibrary {
 
     public static final String MODID = "lmlibrary";
     public static final String NAME = "LMLibrary";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2768,);";
     public static final String MOD_ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
     
