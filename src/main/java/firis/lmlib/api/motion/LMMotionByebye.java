8d2ded8903222ddd4649413211c77f108609bc8f
firis <firis.games.lapismoonlight@gmail.com>
2020-08-22 11:30:46 +0900

    バイバイモーションの腕の向きを調整
    ・アイテムが上を向くように腕を回転

--- src/main/java/firis/lmlib/api/motion/LMMotionByebye.java
+++ src/main/java/firis/lmlib/api/motion/LMMotionByebye.java
@@ -32,10 +32,12 @@ public class LMMotionByebye implements ILMMotion {
 		float lc = ModelBase.mh_cos(ageInTicks * 0.35F);
 		lc *= 0.3F;
 		
+		model.bipedLeftArm.addRotateAngleDegY(-90);
 		model.bipedLeftArm.rotateAngleX = 3.14159F;
 		model.bipedLeftArm.rotateAngleZ = 60F * 3.14159F / 180F;
 		model.bipedLeftArm.addRotateAngleZ(lc);
 		
+		model.bipedRightArm.addRotateAngleDegY(90);
 		model.bipedRightArm.rotateAngleX = -3.14159F;
 		model.bipedRightArm.rotateAngleZ = -60F * 3.14159F / 180F;
 		model.bipedRightArm.addRotateAngleZ(-lc);
